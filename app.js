7cc2d4ad5e3bf9418623b1e587d0c454ffc49d91
peBetrator <fivalit@gmail.com>
2019-05-12 21:03:03 +0300

    12_05_2019

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
 var budgetController = (function() {})();
 
-var UIController = (function() {})();
+var UIController = (function() {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+
+    addButtom: ".add__btn"
+  };
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
+})();
 
 var appController = (function(budgetCtrl, UICtrl) {
-  document.querySelector(".add__btn").addEventListener("click", function() {
-    console.log("button was clicked");
-  });
+  var DOM = UICtrl.getDOMstrings();
+
+  var ctrlAddItem = function() {
+    // 1. Get the field input data
+    var input = UICtrl.getInput();
+    console.log(input);
+  };
+
+  document.querySelector(DOM.addButtom).addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function(event) {
-    if (event.keyCode === 13 || event.which === 13) { //which property for older browsers
+    if (event.keyCode === 13 || event.which === 13) {
+      //which property for older browsers
       console.log("ENTER was pressed");
+      ctrlAddItem();
     }
   });
 })(budgetController, UIController);
